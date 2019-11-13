822cb6ee9d7b9bf046c6a4bbdc841512f108bca9
DeaSTL <jhartway99@gmail.com>
2019-11-13 14:45:30 -0500

    added forge files

---
+++ src/main/java/com/stryphic/quirkymachines/QuirkyMachines.java
@@ -0,0 +1,29 @@
+package com.stryphic.quirkymachines;
+
+import com.stryphic.quirkymachines.util.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class QuirkyMachines
+{
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/com/stryphic/quirkymachines/util/Reference.java
@@ -0,0 +1,7 @@
+package com.stryphic.quirkymachines.util;
+
+public class Reference {
+    public static final String MODID = "quirkymachines";
+    public static final String NAME = "Quirky Machines";
+    public static final String VERSION = "0.0.1";
+}
